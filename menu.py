b30c36d4e3179f6f1aa84e88c326c3094f99b22a
Sofiane ZOUAOUI <sofiane050900@gmail.com>
2021-04-03 19:49:30 +0200

    add: SortData and format

--- menu.py
+++ menu.py
@@ -1,11 +1,12 @@
 from TwitterAPI import TwitterAPI
 from key import API_key,API_secret_key,secret_token,token
 from requests import get
-import pandas as pd
 from bs4 import BeautifulSoup
-import csv
+from datetime import date
 
 
+
+DATE="Menu du vendredi 2 avril 2021"
 
 class MenuRuOnTwitter():
     def __init__(self,API_key,API_secret_key,token,secret_token,URL):
@@ -27,7 +28,15 @@ class MenuRuOnTwitter():
             return True
         return False
 
+    def DateOfToday(self):
+        days=['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche']
+        months=['janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre']
+        Date=date.today()
+        day=days[Date.weekday()]
+        month=months[Date.month-1]
+        MessageDate= "Menu du "+ day +" "+ str(Date.day) + " "+month+ " " + str(Date.year)
 
+        return MessageDate
 
     def Research(self):
         if(self.getPage(self.URL)):
@@ -35,25 +44,64 @@ class MenuRuOnTwitter():
             
             HtmlParser = BeautifulSoup(self.source, 'html.parser')
             listToday=HtmlParser.find('ul',attrs={'class':'slides'})
-            self.Today=listToday.find('h3').getText()
-            listMenu = listToday.find('div',attrs={'class':'content clearfix'})
-            listRepas = listMenu.find_all('ul',attrs={'class':'liste-plats'})
 
+            #print(listToday)
+            
+            indice=0
+            find=False
 
+            listTodays=listToday.find_all('h3')
+            for day in listTodays:
+                if(day.getText() == DATE): # self.DateOfToday()):
+                    find = True
+                    break
 
-            for Repas in listRepas:
-                tuple_data=Repas.find_all('li')
-                for i in range(len(tuple_data)):
-                    if( len(tuple_data[i].getText()) != 0):
-                        self.data.append(tuple_data[i].getText())
+                indice += 1
 
+            if(find):
+                listMenu=listToday.find_all('div',attrs={'class':'content clearfix'})[indice]
+                listRepas = listMenu.find_all('ul',attrs={'class':'liste-plats'})
+
+                for Repas in listRepas:
+                    tuple_data=Repas.find_all('li')
+                    for i in range(len(tuple_data)):
+                        if( len(tuple_data[i].getText()) != 0):
+                            self.data.append(tuple_data[i].getText())
+
+                return True
+
+            else:
+                print(self.DateOfToday()+" don't exits")
+                return False
+        
             
         else:
             print("erreur: getPage()")
+            return False
+
+    def SortData(self):
+        midi=[]
+        soir=[]
+        for menu in self.data:
+            if(menu[:4] == "MIDI"):
+                midi.append(menu[7:])
+            else:
+                soir.append(menu[7:])
+        
+        MENU = f" \033[4m{self.DateOfToday()}\033[0m   \n\n  MIDI \n \t Entrée --> {midi[0]}  \n \t Plat --> {midi[1]}  \n \t Plat Végétarien--> {midi[2]}  \n \t Désert--> {midi[3]} \n\n Soir  \n \t Entrée --> {soir[0]} \n \t Plat --> {soir[1]} \n \t Désert --> {soir[2]}"
+        
+
+        return MENU 
+
+
+
 
 
 URL = "https://www.crous-orleans-tours.fr/restaurant/cafeteria-lahitolle/"
 Bot = MenuRuOnTwitter(API_key,API_secret_key,token,secret_token,URL)
 
-Bot.Research()
+
+
+
+
 
