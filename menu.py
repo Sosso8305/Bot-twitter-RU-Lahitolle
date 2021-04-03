0f219bb23291465d42888986546a40b7c1e2e1b0
Sofiane ZOUAOUI <sofiane050900@gmail.com>
2021-04-03 16:00:54 +0200

    add: Research menu

--- menu.py
+++ menu.py
@@ -1,25 +1,59 @@
 from TwitterAPI import TwitterAPI
 from key import API_key,API_secret_key,secret_token,token
 from requests import get
-from scrapy import Selector
+import pandas as pd
+from bs4 import BeautifulSoup
+import csv
 
-api = TwitterAPI(API_key,API_secret_key,token,secret_token)
+
+
+class MenuRuOnTwitter():
+    def __init__(self,API_key,API_secret_key,token,secret_token,URL):
+        self.api = TwitterAPI(API_key,API_secret_key,token,secret_token)
+        self.URL=URL        
+        
+
+    def PostMessage(self,message):
+        if(len(message)<140):
+            reponse = self.api.request('statuses/update', {'status':message})
+            return True if reponse.status_code == 200  else False
+        return False
+
+
+    def getPage(self,URL):
+        reponse = get(URL)
+        if reponse.status_code == 200:
+            self.source=reponse.text
+            return True
+        return False
+
+
+
+    def Research(self):
+        if(self.getPage(self.URL)):
+            self.data=[]
+            
+            HtmlParser = BeautifulSoup(self.source, 'html.parser')
+            listToday=HtmlParser.find('ul',attrs={'class':'slides'})
+            self.Today=listToday.find('h3').getText()
+            listMenu = listToday.find('div',attrs={'class':'content clearfix'})
+            listRepas = listMenu.find_all('ul',attrs={'class':'liste-plats'})
+
+
+
+            for Repas in listRepas:
+                tuple_data=Repas.find_all('li')
+                for i in range(len(tuple_data)):
+                    if( len(tuple_data[i].getText()) != 0):
+                        self.data.append(tuple_data[i].getText())
+
+            
+        else:
+            print("erreur: getPage()")
+
+
 URL = "https://www.crous-orleans-tours.fr/restaurant/cafeteria-lahitolle/"
-url = "https://fr.wikipedia.org/wiki/Star_Wars"
+Bot = MenuRuOnTwitter(API_key,API_secret_key,token,secret_token,URL)
 
-def PostMessage(message):
-    reponse = api.request('statuses/update', {'status':message})
-    return True if reponse.status_code == 200  else False
-
-
-def getPage(URL,source):
-    reponse = get(URL)
-    if reponse.status_code == 200:
-        source=reponse.text
-        return True
-    return False
-
-
-
-
+Bot.Research()
 
